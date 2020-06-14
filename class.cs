8e6f59b62052dd627f0ab6abd48242e073dd1ea0
Surya Vatsalya <40575068+surya-vatsalya@users.noreply.github.com>
2020-06-14 19:47:50 +0530

    Update class.cs

--- class.cs
+++ class.cs
@@ -2,14 +2,5 @@ namespace CsharpTutorials
 {
   class Class1
   {
-    string name;
-        int age;
-        int gay;
-        string bro;
-        string mom;
-        int mom_age;
-        string sister;
-        int sis_age;
-
   }
 }
