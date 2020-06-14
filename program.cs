ed0f05387f601a53eefbd96ddb1917a242b8041e
Surya Vatsalya <t-suvats@microsoft.com>
2020-06-14 18:56:19 +0530

    commmit

--- program.cs
+++ program.cs
@@ -1,1 +1,18 @@
-hello
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "Hello World!!";
+
+            Console.WriteLine(message);
+        }
+    }
+}
